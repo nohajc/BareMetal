e0345bd0de0960a3b376c13cd07d0ef321f40299
Jan Noha <nohajc@gmail.com>
2015-10-02 18:18:39 +0200

    Modified app.sh and appc.sh to work across sym-links.

--- app.sh
+++ app.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 cd src/BareMetal-OS/programs/
-nasm $1.asm -o ../../../bin/$1.app
+#nasm $1.asm -o ../../../bin/$1.app
+nasm $1.asm -o $1.app
+cd -
+mv src/BareMetal-OS/programs/$1.app bin/
+
 if [ $? -eq 0 ]; then
-cd ../../../bin
+cd bin
 ./bmfs bmfs.image create $1.app 2
 ./bmfs bmfs.image write $1.app
 cd ..
